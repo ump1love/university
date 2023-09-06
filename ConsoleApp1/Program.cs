a833d04026ac8e5a2e3666bd613f82422248b3bb
ump1love <steam.max22.d74.74@gmail.com>
2023-09-06 17:23:05 +0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        char let1 = 'A';
+        char let2 = 'B';
+        Console.Write($"first char is: {let1}, and {let2} is the second one\n"); // this is just a print line
+        Console.Write("and here is your char: {0}", Console.ReadLine());
+        /*
+        and this is just a comment
+        same here
+        */
+    }
+}
