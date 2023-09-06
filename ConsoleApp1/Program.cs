d1fe8f8538b5f0c1f10de2d52a43243921f7c085
ump1love <steam.max22.d74.74@gmail.com>
2023-09-06 17:32:58 +0300

    added something new in develop branch

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,8 @@ class Program
         char let1 = 'A';
         char let2 = 'B';
         Console.Write($"first char is: {let1}, and {let2} is the second one\n"); // this is just a print line
-        Console.Write("and here is your char: {0}", Console.ReadLine());
+        Console.WriteLine("There is something new");
+        Console.WriteLine("Changes for *develop* branch");
         /*
         and this is just a comment
         same here
